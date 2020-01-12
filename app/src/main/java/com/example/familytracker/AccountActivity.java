5e88b44f9e342d12514dc38ed488a96428e29863
Jan <janvanderwant@gmail.com>
2020-01-12 20:42:37 +0100

    activity switch toegevoegd en request logica naan volley klasse verplaatst

--- app/src/main/java/com/example/familytracker/AccountActivity.java
+++ app/src/main/java/com/example/familytracker/AccountActivity.java
@@ -1,43 +1,36 @@
 package com.example.familytracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.familytracker.Model.Account;
 import com.example.familytracker.buisnessLayer.AccountManager;
 import com.example.familytracker.buisnessLayer.BuisnessController;
 
-public class MainActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     private Button addButton;
     private BuisnessController buisnessController;
     private AccountManager accountManager;
-    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account);
 
-        context = this.getApplicationContext();
-        buisnessController = new BuisnessController(context);
+        buisnessController = new BuisnessController(this.getApplicationContext());
         accountManager = buisnessController.getAccountManager();
 
-        addButton = (Button) findViewById(R.id.bt_main_createnewfamilyaccount) ;
+        addButton = (Button) findViewById(R.id.bt_account_create_account) ;
         addButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AccountActivity.class));
+
             }
         });
     }
-
-
 }
